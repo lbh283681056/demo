d5103d9e5dc77a68c026912807355ebd88ee5bb8
林炳煌 <linbinghuang@linbinghuangdeMacBook-Pro.local>
2018-05-03 14:52:11 +0800

    加入登入 分享 优化七牛云

---
+++ app/src/main/java/com/base/demo/MediaPlayerTest.java
@@ -0,0 +1,8 @@
+package com.base.demo;
+
+/**
+ * Created by linbinghuang on 2018/5/3.
+ */
+
+public class MediaPlayerTest {
+}
---
+++ lib-live/src/main/java/com/base/live/livehelp/qny/IQnyCallBack.java
@@ -0,0 +1,8 @@
+package com.base.live.livehelp.qny;
+
+/**
+ * Created by linbinghuang on 2018/5/2.
+ */
+
+public class IQnyCallBack {
+}
